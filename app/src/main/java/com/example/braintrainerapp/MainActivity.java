dc8aa8fe6c00156fa0a338ac5724431993a03a0e
Anjali Singh <muskan.anjali.singh@gmail.com>
2020-06-28 11:45:31 +0530

    Front Page

--- app/src/main/java/com/example/braintrainerapp/MainActivity.java
+++ app/src/main/java/com/example/braintrainerapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.braintrainerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton;
+    public void start(View view)
+    {
+        startButton.setVisibility(View.INVISIBLE);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //adds items to action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton=(Button)findViewById(R.id.startButton);
     }
 }
